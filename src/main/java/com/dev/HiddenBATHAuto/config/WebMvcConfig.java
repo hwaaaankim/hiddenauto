2d4df203a6edbd0b8949f73b80aacb672ae36b5a
thramania <thramania@hanmail.net>
2025-06-16 12:04:57 +0900

    localDateTime bug fix

--- src/main/java/com/dev/HiddenBATHAuto/config/WebMvcConfig.java
+++ src/main/java/com/dev/HiddenBATHAuto/config/WebMvcConfig.java
@@ -1,13 +1,19 @@
 package com.dev.HiddenBATHAuto.config;
 
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -33,5 +39,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations(resourcePath);
+    }
+    
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                ObjectMapper objectMapper = ((MappingJackson2HttpMessageConverter) converter).getObjectMapper();
+                objectMapper.registerModule(new JavaTimeModule());
+            }
+        }
     }
 }
